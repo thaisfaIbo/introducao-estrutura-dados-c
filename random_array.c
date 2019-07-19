b46015d84ffe60a0fb123c462e848d26128c5ad3
thaisfalbo <thaisfalbo@outlook.com>
2019-07-18 21:59:22 -0300

    Adiciona lib em random_array.c

--- random_array.c
+++ random_array.c
@@ -3,6 +3,7 @@
 // NOTA: O método srand não foi utilizado pq caso necessite
 // criar várias arrays com mesmo tipo, elas terão os mesmos valores
 #include <stdlib.h>
+#include <stdio.h>
 
 void random_int_array(int tamanho, int* vetor)  {
   for (int i = 0; i < tamanho; ++i) {
