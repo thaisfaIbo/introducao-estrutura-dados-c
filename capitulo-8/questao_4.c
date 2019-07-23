6745e424bd42c34adce109e202e7c070b952b261
thaisfalbo <thaisfalbo@outlook.com>
2019-07-23 16:48:33 -0300

    Refatoração

--- capitulo-8/questao_4.c
+++ capitulo-8/questao_4.c
@@ -1,15 +1,10 @@
 #include <stdio.h>
 
-void shift_string(char* str) {
-  for (int i = 0; str[i] != '\0'; ++i) {
-    if ((str[i] >= 'A' && str[i] <= 'Z') || (str[i] >= 'a' && str[i]<= 'z')) {
-      if (str[i] == 'z')
-        str[i] = 'a';
-      else if (str[i] == 'Z')
-        str[i] = 'A';
-      else
-        str[i]++;
-    }
+void shift_string(char* s) {
+  for (int i = 0; s[i] != '\0'; ++i) {
+    if ((s[i] >= 'A' && s[i] < 'Z') || (s[i] >= 'a' && s[i] < 'z')) s[i]++;
+    if (s[i] == 'z') s[i] = 'a';
+    if (s[i] == 'Z') s[i] = 'A';
   }
 }
 
