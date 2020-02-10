47d8881bf3721b87e4ce6791915ee928a265ca99
Thaís Falbo <thaisfalbo@outlook.com>
2020-02-09 23:45:15 -0300

    Refatoração

--- capitulo-11/questao_2.c
+++ capitulo-11/questao_2.c
@@ -1,11 +1,10 @@
 #include <stdlib.h>
-#include <stdio.h>
 
 typedef struct pessoa Pessoa;
 
 struct pessoa
 {
-  char nome[10];
+  char nome[81];
   int dia, mes, ano;
 };
 
@@ -14,12 +13,10 @@ static int compara_pessoa(const void *p1, const void *p2)
   Pessoa *info = (Pessoa *)p1;
   Pessoa **elem = (Pessoa **)p2;
 
-  if ((*elem)->ano > info->ano)
-    return 1;
-  else if ((*elem)->ano < info->ano)
-    return -1;
-  else if ((*elem)->ano == info->ano)
-    return 0;
+  int ano = info->ano - (*elem)->ano;
+  int mes = info->mes - (*elem)->mes;
+  int dia = info->dia - (*elem)->dia;
+  return (ano ? ano : (mes ? mes : dia));
 }
 
 Pessoa *busca(int tamanho_vetor, Pessoa **vetor, int dia, int mes, int ano)
